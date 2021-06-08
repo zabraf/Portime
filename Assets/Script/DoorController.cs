11b410ae1da412be749c412481d28e94c2e402e9
Zabraf <lopesraphi@gmail.com>
2021-06-08 23:15:24 +0200

    stuff

--- Assets/Script/DoorController.cs
+++ Assets/Script/DoorController.cs
@@ -5,26 +5,45 @@ using UnityEngine;
 public class DoorController : Activable
 {
     public bool Resverse = false;
+    public int nbrActivabe = 1;
+    private int nb;
+    private void Start()
+    {
+        nb = nbrActivabe;
+    }
+    public void Restart()
+    {
+        nb = nbrActivabe;
+    }
     public override void Activate()
     {
-        if (Resverse) { 
-            this.transform.gameObject.SetActive(true);
-        }
-        else
-        {
-            this.transform.gameObject.SetActive(false);
+        nb--;
+        if(nb == 0)
+        { 
+            if (Resverse) { 
+                this.transform.gameObject.SetActive(true);
+            }
+            else
+            {
+                this.transform.gameObject.SetActive(false);
+            }
         }
     }
 
     public override void Desactivate()
     {
-        if (Resverse)
+        if (nb == 0)
         {
-            this.transform.gameObject.SetActive(false);
+            if (Resverse)
+            {
+                this.transform.gameObject.SetActive(false);
+            }
+            else
+            {
+                this.transform.gameObject.SetActive(true);
+            }
         }
-        else
-        {
-            this.transform.gameObject.SetActive(true);
-        }
+        nb++;
+       
     }
 }
